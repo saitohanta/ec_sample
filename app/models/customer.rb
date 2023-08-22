17d4031bf9686824de7f25519746bb263ef5141b
saitohanta <osyogatu17@icloud.com>
2023-08-22 01:17:02 +0000

    [Update] customer

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -16,4 +16,12 @@ class Customer < ApplicationRecord
   validates :address, presence: true
   validates :telephone_number, presence: true
 
+  def full_name
+    last_name + '' + first_name
+  end
+
+  def full_name_kana
+    last_name_kana + '' + first_name_kana
+  end
+
 end
